977b424506788ec164f877c380be11e4ea4cb2e1
connor paterick <cwpaterick@gmail.com>
2020-01-12 08:40:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ var greetSpecialGuest = "" + "Hello, Neil deGrasse Tyson" + "!";
 
 var topic = "space";
 
-var conversation = "let's talk about space"
+var conversation = "let's talk about space."
 
